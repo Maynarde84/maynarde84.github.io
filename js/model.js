f5beb0e89b10f13340e810cd81b7651c559d1d89
Maynarde84 <maynarde.sc2@gmail.com>
2015-09-13 17:51:51 +0930

    Changed site to neighbourhood map project

---
+++ js/model.js
@@ -0,0 +1,42 @@
+//Cafe objects / JSON
+var cafes = [{
+    name: "Whipped Bake Bar Cafe",
+    address: "35 Semaphore Rd, Semaphore SA 5019",
+    lat: -34.838411,
+    lng: 138.482782,
+	rating: '7/10',
+    tag: 'whipped',
+	website: 'http://www.whipped.com.au/'
+}, {
+    name: "Swedish Tarts Patisserie",
+    address: "40 Semaphore Rd, Semaphore SA 5019",
+    lat: -34.838920,
+    lng: 138.483038,
+	rating: '8/10',
+    tag: 'swedishtarts',
+	website: 'http://www.swedishtarts.com/'
+}, {
+	name: "Cafe Froot",
+	address: "36B Semaphore Rd, Semaphore SA 5019",
+	lat: -34.839008,
+	lng: 138.482649,
+	rating: '6.5/10',
+	tag: 'froot',
+	website: null
+}, {
+    name: "Sarah's Sisters",
+    address: "117 Semaphore Rd, Semaphore SA 5019",
+    lat: -34.839532,
+    lng: 138.488407,
+	rating: '5/10',
+    tag: 'sarahssisters',
+	website: null
+}, {
+    name: "Sully's on Semaphore",
+    address: "114A Semaphore Rd, Semaphore SA 5019",
+    lat: -34.839790,
+    lng: 138.487878,
+	rating: '7/10',
+    tag: 'sullys',
+	website: 'http://www.sullysonsemaphore.com.au/'
+}];
